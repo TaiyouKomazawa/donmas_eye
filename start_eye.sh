6340591bbf1ee3965e00ca01217e3254cbd301c5
Taiyou Komazawa <taiyou24690@gmail.com>
2022-08-21 03:52:45 +0900

    [Docs] コマンドの入力例を追加

--- start_eye.sh
+++ start_eye.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# こんな感じで、自動起動に書き込んでおく sh /(ドンマスアイの保存場所のパス)/donmasu_eye/start_eye.sh (固定したIPアドレス) (使用するポート)
+# 例 : sh /home/ubuntu/ドキュメント/donmasu_eye/start_eye.sh 192.168.0.34 35001
+
 SCR_DIR=$(cd $(dirname $0); pwd)
 cd $SCR_DIR
 
